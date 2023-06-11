3635a281877514aeadd2066e5748420c8eca8798
jitendra25Aug <jitendra52aug@gmail.com>
2023-06-11 21:34:27 +0530

    fixed login issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { withAuthenticationRequired } from "@auth0/auth0-react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthWrapper, Error, Home, Login, PrivateRoute, SharedLayout, SingleRepo } from "./pages";
 
 const App = () => {
 
+  const ProtectedRoute = ({ component, ...args }: any) => {
+    const Component = withAuthenticationRequired(component, args);
+    return <Component />;
+  };
+
   return (
     <AuthWrapper>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<SharedLayout />} >
+          <Route path="/" element={<ProtectedRoute component={SharedLayout} />} >
             <Route index element={<PrivateRoute> <Home /> </PrivateRoute>} />
             <Route path=":owner/:repoName" element={<PrivateRoute> <SingleRepo /> </PrivateRoute>} />
           </Route>
